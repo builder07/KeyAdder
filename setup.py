956f834fc16a534071607826933a466a2d401267
RAED <rrrr53@hotmail.com>
2019-07-07 21:27:28 +0300

    Upload missing file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+pkg = 'Extensions.Biscotto'
+setup (name = 'enigma2-plugin-extensions-Biscotto',
+       version = '2.0-r2',
+       description = 'Add BISS, PowerVU, IRDETO and Tandberg key to current service',
+       packages = [pkg],
+       package_dir = {pkg: 'plugin'},
+       package_data = {pkg: ['plugin.png']},
+       cmdclass = setup_translate.cmdclass, # for translation
+      )
