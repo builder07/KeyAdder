4f7c9d8822642d5c98af0d35ce9681d8704e6087
RAED <rrrr53@hotmail.com>
2019-07-13 22:53:26 +0300

    Fix setup file + Update plugin version

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setup_translate
 
 pkg = 'Extensions.KeyAdder'
 setup (name = 'enigma2-plugin-extensions-keyadder',
-       version = '2.1',
+       version = '2.2',
        description = 'Add BISS, PowerVU, Irdeto and Tandberg keys to current service.',
        packages = [pkg],
        package_dir = {pkg: 'plugin'},
-       package_data = {pkg: ['plugin.png', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
+       package_data = {pkg: ['plugin.png', '*/*.png', 'locale/*/LC_MESSAGES/*.mo', 'VirtualKeyBoard_Icons/vkskin.xml', 'VirtualKeyBoard_Icons/*/*.png']},
        cmdclass = setup_translate.cmdclass, # for translation
       )
